56bb5574196fcff4a68fb4d733f067d5ddcd349f
nicconicco <nicolaugalves@gmail.com>
2018-12-06 13:20:12 -0200

    [ Seguranca ]

---
+++ seguranca/examplo-03.php
@@ -0,0 +1,9 @@
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
+ <form action="cadastrar.php" method="post">
+ 	
+ 	<div class="g-recaptcha" data-sitekey="6LcxGn8UAAAAAJiGXwcHz8Mn86pE5M6FUcldiVKd"></div>
+ 	<input type="email" name="inputEmail">
+ 	<button type="submit">Enviar</button>
+
+ </form>
